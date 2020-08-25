f67440e9bb1d12d234ae56488551129252de8a01
Chunfu Zhang <czhang611@gmail.com>
2020-08-25 10:52:41 -0400

    Comment out unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side JavaScript is loading');
+// console.log('Client side JavaScript is loading');
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
