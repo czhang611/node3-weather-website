3709cad88828e14a45f02026a6bccacc024c4915
Chunfu Zhang <czhang611@gmail.com>
2020-08-25 12:18:14 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,8 @@ weatherForm.addEventListener ('submit', (e) => {
     e.preventDefault();
     
     const location = search.value;
-    const url = 'http://localhost:3000/weather?address=' + location;
+    // const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
 
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
